ab2cbd1b0cf9373af9282fee0e618ba23ed74eeb
Alexandra Sarafoglou <alexandra.sarafoglou@gmail.com>
2024-05-21 12:56:58 +0200

    updated documentation
    updated documentation and function addSeven to illustrate how to display a list of items within the help file.

--- PlayWithNumbers/R/package_functions.R
+++ PlayWithNumbers/R/package_functions.R
@@ -3,7 +3,11 @@
 #' @description Adds seven to any number or vector given to the function.
 #' 
 #' @param x numeric. A single number or a vector with data.
-#' @return A single number or a vector with 7 added to each of its elements.
+#' @return A data.frame containing the following columns:
+#' \describe{
+#'  \item{old_numbers}{numeric. A vector of numbers specified by the user.}
+#'  \item{new_numbers}{numeric. A vector of numbers specified by the user with 7 added to each of its elements.}
+#' }
 #' 
 #' @examples 
 #' # add 7 to a vector of numbers
@@ -11,9 +15,15 @@
 #' addSeven(x)
 #' # add 7 to a single number
 #' addSeven(3)
-#' @references Sarafoglou, A. (2024). How to add 7 to a number. Science, 10, 206-208.
+#' @references 
+#' * Sarafoglou, A. (2024). How to add 7 to a number. Science, 10, 206-208.
+#' * Sarafoglou, A. (2023). What is addition, when is it useful? Science, 2, 100-168.
 #' @export
-addSeven <- function(x) {x + 7}
+addSeven <- function(x) {
+  dat <- data.frame(old_numbers = x,
+                    new_numbers = x + 7)
+  return(dat)
+  }
 
 #' @export
 addThree <- function(x) {x + 3}
@@ -25,7 +35,7 @@ squareNumber <- function(x){x^2}
 visualizeTransformations <- function(vector_with_numbers){
   
   df <- data.frame(orig        = rep(vector_with_numbers, 3), 
-                   transformed = c(addSeven(vector_with_numbers),
+                   transformed = c(addSeven(vector_with_numbers)$new_numbers,
                                    addThree(vector_with_numbers),
                                    squareNumber(vector_with_numbers)),
                    group       = rep(1:3, each = length(vector_with_numbers)))
