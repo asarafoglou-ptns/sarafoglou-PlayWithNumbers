bfac623542797f5dbf8ed8d327319fdfa295d389
Alexandra Sarafoglou <alexandra.sarafoglou@gmail.com>
2024-05-23 12:03:49 +0200

    created demo data
    created demo data "numbers"

---
+++ PlayWithNumbers/R/numbers.R
@@ -0,0 +1,15 @@
+#' @title Numbers
+#' 
+#' @description A data frame which features natural numbers, integers, and real numbers
+#' 
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'  \item{natural_numbers}{numeric. Natural numbers ranging from 1 to 100}
+#'  \item{integers}{numeric. Integers ranging from -49 to 50.}
+#'  \item{real_numbers}{numeric. Real numbers drawn from a normal distribution with mean 0 and standard deviation of 30.}
+#' }
+#' @source Sarafoglou, A. (2024). How to add 7 to a number. Science, 10, 206-208.
+#' 
+#' @examples
+#' data(numbers)
+"numbers"
